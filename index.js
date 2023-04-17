2487292ac27b4ccac3c31e8b65f06b3b424c280c
msarvanm <muthukus@roehampton.ac.uk>
2023-04-17 06:24:28 +0530

    files updated

--- index.js
+++ index.js
@@ -1,58 +1,14 @@
-import express from 'express';
-import authRoutes from "./routes/auth.js"
-import userRoutes from "./routes/users.js"
-import postRoutes from "./routes/posts.js"
-import simRoutes from "./routes/sims.js"
-import patientRoutes from "./routes/patients.js"
-import admissionRoutes from "./routes/admission.js"
-import allergyRoutes from "./routes/allergy.js"
-import historyRoutes from "./routes/history.js"
-import cookieParser from "cookie-parser";
-import multer from 'multer';
-import bodyParser from 'body-parser';
-import * as dotenv from 'dotenv'
-
+const express = require("express");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const app = express ();
-app.use(cookieParser());
-app.use (express.json())
-app.use(bodyParser.urlencoded({extended: true}));
-const PORT = process.env.PORT || 8000;
+const app = express();
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '../client/public/upload');
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now()+file.originalname);
-    }
-  })
-  
-  const upload = multer({ storage });
+const PORT = process.env.PORT || 9000;
 
-app.post('/api/upload', upload.single('file'), function (req, res){
-    const file = req.file;
-    res.status(200).json(file.filename);
+app.get("/", (req,res)=> {
+    res.send("Hello there! Api is working")
 })
 
-app.use ("/api/auth", authRoutes)
-app.use ("/api/users", userRoutes)
-app.use ("/api/posts", postRoutes)
-app.use ("/api/sims", simRoutes)
-app.use ("/api/patients", patientRoutes)
-app.use ("/api/admissions", admissionRoutes)
-app.use ("/api/allergys", allergyRoutes)
-app.use ("/api/historys", historyRoutes)
-
-app.get("/test2", (res, req)=>{
-    res.send("from controller");
-})
-
-
-
-
-app.listen(PORT, ()=>{
-    console.log("Conneccted")
-});
+app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
