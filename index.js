7fa9fcc5738449579d3a0558dd2599908237e736
msarvanm <muthukus@roehampton.ac.uk>
2023-04-18 02:05:19 +0530

    code updated

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bodyParser = require ('body-parser');
+const cors = require ('cors');
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: 'root',
+    password: 'password',
+    database: 'crud_contact'
+})
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = process.env.PORT || 9000;
 
 app.get("/", (req,res)=> {
     res.send("Hello there! Api is working well")
+})
+
+app.get("/test", (req,res)=> {
+    res.send("Hello there! Test Api is working well")
 })
 
 app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
