f9236589fe64c7421f860baa0819982d7b4ddbc0
msarvanm <muthukus@roehampton.ac.uk>
2023-04-17 06:28:33 +0530

    files updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.get("/", (req,res)=> {
-    res.send("Hello there! Api is working")
+    res.send("Hello there! Api is working well")
 })
 
 app.listen(PORT, () => console.log(`Sever is running port ${PORT} ...`));
